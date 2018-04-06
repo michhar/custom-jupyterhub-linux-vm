9732abbfe694b189d5ea69e775a9ad7a7ff11686
Micheleen Harris <michhar@microsoft.com>
2018-04-06 12:59:07 -0700

    updated imaage used in install script

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run -d -v /var/run/docker.sock:/var/run/docker.sock -p 5555:5555 -p 80:7842 -p 8787:8787 -p 8786:8786 -p 8788:8788 --expose=8788 rheartpython/cvopenhack:unix
+sudo docker run -d -v /var/run/docker.sock:/var/run/docker.sock -p 5555:5555 -p 80:7842 -p 8787:8787 -p 8786:8786 -p 8788:8788 --expose=8788 rheartpython/cvopenhack:latest
