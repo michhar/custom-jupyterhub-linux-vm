0ea196ee74354825d9ef0fbf3ecb94b454f6e602
Micheleen Harris <michhar@microsoft.com>
2018-04-07 14:09:42 -0700

    install script fixes

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run -d -p 5555:5555 -p 80:7842 -p 8787:8787 -p 8786:8786 -p 8788:8788 -v ~/dev/:/root/sharedfolder --expose=8788  rheartpython/cvopenhack:latest
+sudo docker run -it -v /var/run/docker.sock:/var/run/docker.sock -p 8788:8788 --expose=8788 rheartpython/cvopenhack:latest
