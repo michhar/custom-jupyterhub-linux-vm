6222deed3d0ccfa9b4d8b91b6fb813f80ebbb004
michhar <michhar@microsoft.com>
2018-11-05 12:27:14 -0800

    updated docker run command and image gpu

--- install_gpu_torch1.0.sh
+++ install_gpu_torch1.0.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo nvidia-docker run -d -p 5555:5555 -p 80:7842 -p 8788:8788 -v ~/dev/:/root/sharedfolder --expose=8788  rheartpython/cvdeep_gpu:torch1.0
+#sudo nvidia-docker run -d -p 5555:5555 -p 80:7842 -p 8788:8788 -v ~/dev/:/root/sharedfolder --expose=8788  rheartpython/cvdeep_gpu:torch1.0alpha
+
+sudo nvidia-docker run -d -v /var/run/docker.sock:/var/run/docker.sock -p 5555:5555 -p 80:7842 -p 8788:8788 --expose=8788 rheartpython/cvdeep_gpu:torch1.0alpha
