da96f136ca48616cb9b5c6dc6039d0a9174420fa
Micheleen Harris <michhar@microsoft.com>
2018-11-05 17:27:33 -0800

    revert to prev nvidia-docker cmd

--- install_gpu_torch1.0.sh
+++ install_gpu_torch1.0.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=0 -d -p 5555:5555 -p 80:7842 -p 8788:8788 -v ~/dev/:/root/sharedfolder --expose=8788  rheartpython/cvdeep_gpu:torch1.0alpha
+sudo nvidia-docker run -d -p 5555:5555 -p 80:7842 -p 8788:8788 -v ~/dev/:/root/sharedfolder --expose=8788  rheartpython/cvdeep_gpu:torch1.0alpha
