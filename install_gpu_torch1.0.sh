f72b8ab4b8e2690afe0e5e6c287c58010478d6d0
michhar <michhar@microsoft.com>
2019-03-05 07:21:49 -0800

    simplifying script

--- install_gpu_torch1.0.sh
+++ install_gpu_torch1.0.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo nvidia-docker run -d -p 5555:5555 -p 80:7842 -p 8788:8788 -v ~/dev/:/root/sharedfolder --expose=8788  rheartpython/cvdeep_gpu:latest
+sudo nvidia-docker run -d -v /var/run/docker.sock:/var/run/docker.sock -p 8788:8788 --expose=8788 rheartpython/cvdeep_gpu:latest
